cb71bb19df715be04a224eaef072d17226dfa1dd
Jack Thorne <jack@kilmer.co>
2026-04-08 11:12:00 -0500

    perf: Buffer bit writer output to reduce IO write calls
    Add 4KB output buffer to BitWriter. Complete bytes are accumulated in the
    buffer and flushed in bulk via IO.write instead of calling IO.write_byte
    per byte. Defer flushing until the bit buffer has 32+ bits to reduce
    flush frequency.

--- src/z/bits/bit_writer.cr
+++ src/z/bits/bit_writer.cr
@@ -1,15 +1,20 @@
 module Z
   class BitWriter
+    OUTPUT_BUFFER_SIZE = 4096
+
     @buffer : UInt64 = 0_u64
     @bits : Int32 = 0
+    @out_buf : Bytes = Bytes.new(OUTPUT_BUFFER_SIZE)
+    @out_pos : Int32 = 0
 
     def initialize(@io : IO)
     end
 
+    @[AlwaysInline]
     def write_bits(value : UInt32, n : Int32) : Nil
       @buffer |= value.to_u64 << @bits
       @bits += n
-      flush_complete_bytes
+      flush_complete_bytes if @bits >= 32
     end
 
     def write_bit(b : Bool) : Nil
@@ -30,27 +35,38 @@ module Z
     def write_bytes(slice : Bytes) : Nil
       align_to_byte
       flush_all
+      flush_out_buf
       @io.write(slice)
     end
 
     def flush : Nil
       align_to_byte
       flush_all
+      flush_out_buf
     end
 
     def flush_all : Nil
+      flush_complete_bytes
+      flush_out_buf
+    end
+
+    @[AlwaysInline]
+    private def flush_complete_bytes : Nil
       while @bits >= 8
-        @io.write_byte((@buffer & 0xFF).to_u8)
+        @out_buf[@out_pos] = (@buffer & 0xFF).to_u8
+        @out_pos += 1
         @buffer >>= 8
         @bits -= 8
+        if @out_pos >= OUTPUT_BUFFER_SIZE
+          flush_out_buf
+        end
       end
     end
 
-    private def flush_complete_bytes : Nil
-      while @bits >= 8
-        @io.write_byte((@buffer & 0xFF).to_u8)
-        @buffer >>= 8
-        @bits -= 8
+    private def flush_out_buf : Nil
+      if @out_pos > 0
+        @io.write(@out_buf[0, @out_pos])
+        @out_pos = 0
       end
     end
   end
